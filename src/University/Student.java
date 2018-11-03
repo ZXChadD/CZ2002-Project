304fd4bc81a10a827389ef545d8ff78b84dd462c
dexleow96 <43750681+dexleow96@users.noreply.github.com>
2018-11-03 23:22:24 +0800

    Skeleton?

--- src/University/Student.java
+++ src/University/Student.java
@@ -1,4 +1,5 @@
 package University;
+import java.util.Scanner;
 
 public class Student
 {
@@ -15,14 +16,79 @@ public class Student
 		this.faculty = faculty;
 	}
 
-	public void regCourse()
+	public void regCourse(Course[] course, int courseCount, int courseSlot)
 	{
-		
+		int i;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Course ID: ");
+		int courseChoice = sc.nextInt();
+		for(i=0;i<courseCount;i++)
+		{
+			if(course[i].getCourseId()==courseChoice)
+			{
+				//if(lec slot < max slot)
+					//if student id exist
+						System.out.println("You are already registered for this course.");
+					else
+						//if(lab is available)
+							//choose lab group
+							//if(lab slot < max slot)
+								//add student id to lab
+								//choose tut group
+								//if(tut slot < max slot)
+									//add student id to tut
+								else
+									System.out.println("No more vacancies.");
+							else
+								System.out.println("No more vacancies.");
+						//else if(tut is available)
+						//choose tut group
+							//if(tut slot < max slot)
+								//add student id to tut slot
+							else
+								System.out.println("No more vacancies.");	
+				else
+					System.out.println("No more vacancies. ");
+			}
+		}
+		if(i>=courseCount)
+			System.out.println("Please enter a valid Course ID.");
 	}
 	
-	public void checkAvail()
+	public void checkAvail(Course[] course, int courseCount)
 	{
-		
+		int i;
+		System.out.println("Enter Course ID.");
+		Scanner sc = new Scanner(System.in);
+		int courseChoice = sc.nextInt();
+		for(i=0;i<courseCount;i++) {
+			if(course[i].getCourseId()==courseChoice) {
+				System.out.println("1. Tutorial group availability");
+				System.out.println("2. Lab group availability");
+				System.out.println("Enter your choice: ");
+				int tutlabChoice = sc.nextInt();
+				switch (tutlabChoice) {
+				case 1:
+					System.out.println("Enter a tutorial group (1 to 5).");
+					int tutChoice = sc.nextInt();
+					if(/*valid*/)
+						System.out.println("Slots left: " + /* max slots - slot */ + "/" + /*max slots*/);
+					else
+						System.out.println("Please enter a valid tutorial group (1 to 3).");
+				case 2:
+					System.out.println("Enter a lab group (1 to 5).");
+					int labChoice = sc.nextInt();
+					if(/*valid*/)
+						System.out.println("Slots left: " + /* max slots - slot */ + "/" + /*max slots*/);
+					else
+						System.out.println("Please enter a valid lab group (1 to 3).");
+				default:
+					System.out.println("Please enter 1 or 2.");
+				}
+			}
+		}
+		if(i>=courseCount)
+			System.out.println("Please enter a valid Course ID.");
 	}
 	
 	public void printTranscript()
--- src/University/UniversityApp.java
+++ src/University/UniversityApp.java
@@ -27,7 +27,7 @@ public class UniversityApp {
 								System.out.println("Please enter a valid Professor ID.");
 							else
 								//prof function
-							break;
+								break;
 						} while ();
 					
 				default: System.out.println("Please enter a valid choice.");
@@ -36,10 +36,76 @@ public class UniversityApp {
 	}
 	
 	public void studFunc() {
-		
+		Scanner sc = new Scanner(System.in);
+		Student stud = new Student(0, null, null, null, null, null);
+		System.out.println("List of actions:");
+		System.out.println("1: Register for a course");
+		System.out.println("2: Check availability for a tutorial/lab group");
+		System.out.println("3: Print transcript");
+		System.out.println("4: Exit");
+		System.out.println("Please enter your choice: ");
+		do {
+			int studChoice = sc.nextInt();
+			switch(studChoice)
+			case 1: 
+				stud.regCourse(course, courseCount, courseSlot);
+				break;
+			case 2:
+				stud.checkAvail(course, courseCount);
+				break;
+			case 3:
+				stud.printTranscript();
+				break;
+			case 4:
+				System.out.println("Thank you for using the app");
+				break;
+			default:
+				System.out.println("Please enter a valid action.");
+		}while(studChoice>4 || studChoice<1)
 	}
 	
 	public void profFunc() {
-		
+		Scanner sc = new Scanner(System.in);
+		Professor prof = new Professor(0, null, null, null, null, null, null);
+		System.out.println("List of actions:");
+		System.out.println("1: Add a student");
+		System.out.println("2: Add a course");
+		System.out.println("3: Print student list");
+		System.out.println("4: Enter course assessment component weightage");
+		System.out.println("5: Enter coursework mark");
+		System.out.println("6: Enter exam mark");
+		System.out.println("7: Print course statistics");
+		System.out.println("8: Exit");
+		System.out.println("Please enter your choice: ");
+		do {
+			int profChoice = sc.nextInt();
+			switch(profChoice)
+			case 1: 
+				prof.addStud(stud, count);
+				break;
+			case 2:
+				prof.addCourse();
+				break;
+			case 3:
+				prof.printStud();
+				break;
+			case 4:
+				prof.weightage();
+				break;
+			case 5:
+				prof.cwMark();
+				break;
+			case 6:
+				prof.examMark();
+				break;
+			case 7:
+				prof.printStats();
+				break;
+			case 8:
+				System.out.println("Thank you for using the app");
+				break;
+			default:
+				System.out.println("Please enter a valid action.");
+		}while(profChoice>8 || profChoice<1)
 	}
 }
